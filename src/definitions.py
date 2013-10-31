4e9554372f2a3ea2177e5859f12e88263b61ac22
Stu Fletcher <mail@stuliveshere.com>
2013-10-31 15:07:32 +1000

    cleanup

--- src/definitions.py
+++ src/definitions.py
@@ -157,8 +157,10 @@ class segy_file_header(tb.IsDescription):
     TFH38 = tb.StringCol(80)
     TFH39 = tb.StringCol(80)
     TFH40 = tb.StringCol(80)
+    
 
-segy_binary_header = np.dtype([
+
+segy_binary_header_dtype = np.dtype([
     ('jobid', '>i4'),
     ('lino', '>i4'),
     ('reno', '>i4'),
@@ -194,7 +196,7 @@ segy_binary_header = np.dtype([
 ])
 
 
-segy_textual_file_header = np.dtype([
+segy_textual_header_dtype = np.dtype([
     ('TFH1', '<a80'),
     ('TFH2', (np.str_,   80)),
     ('TFH3', (np.str_,   80)),
@@ -235,6 +237,100 @@ segy_textual_file_header = np.dtype([
     ('TFH38', (np.str_,   80)),
     ('TFH39', (np.str_,   80)),
     ('TFH40', (np.str_,   80)),
+])
+
+segy_trace_header_dtype = np.dtype([
+    ('tracl',  '>i4'),
+    ('tracr',  '>i4'),
+    ('fldr',   '>i4'),
+    ('tracf',  '>i4'),
+    ('ep',     '>i4'),
+    ('cdp',    '>i4'),
+    ('cdpt',   '>i4'),
+    ('trid',   '>i2'),
+    ('nvs',    '>i2'),
+    ('nhs',    '>i2'),
+    ('duse',   '>i2'),
+    ('offset', '>i4'),
+    ('gelev',  '>i4'),
+    ('selev',  '>i4'),
+    ('sdepth', '>i4'),
+    ('gdel',   '>i4'),
+    ('sdel',   '>i4'),
+    ('swdep',  '>i4'),
+    ('gwdep',  '>i4'),
+    ('scalel', '>i2'),
+    ('scalco', '>i2'),
+    ('sx',     '>i4'),
+    ('sy',     '>i4'),
+    ('gx',     '>i4'),
+    ('gy',     '>i4'),
+    ('counit', '>i2'),
+    ('wevel',   '>i2'),
+    ('swevel',  '>i2'),
+    ('sut',     '>i2'),
+    ('gut',     '>i2'),
+    ('sstat',   '>i2'),
+    ('gstat',   '>i2'),
+    ('tstat',   '>i2'),
+    ('laga',    '>i2'),
+    ('lagb',    '>i2'),
+    ('delrt',   '>i2'),
+    ('muts',    '>i2'),
+    ('mute',    '>i2'),
+    ('ns',      '>i2'),
+    ('dt',      '>i2'),
+    ('gain',    '>i2'),
+    ('igc',    '>i2'),
+    ('igi',    '>i2'),
+    ('corr',   '>i2'),
+    ('sfs',    '>i2'),
+    ('sfe',    '>i2'),
+    ('slen',   '>i2'),
+    ('styp',   '>i2'),
+    ('stas',   '>i2'),
+    ('stae',   '>i2'),
+    ('tatyp',  '>i2'),
+    ('afilf',  '>i2'),
+    ('afils',  '>i2'),
+    ('nofilf', '>i2'),
+    ('nofils', '>i2'),
+    ('lcf',    '>i2'),
+    ('hcf',    '>i2'),
+    ('lcs',    '>i2'),
+    ('hcs',    '>i2'),
+    ('year',   '>i2'),
+    ('day',    '>i2'),
+    ('hour',   '>i2'),
+    ('minute', '>i2'),
+    ('sec',    '>i2'),
+    ('timbas', '>i2'),
+    ('trwf',   '>i2'),
+    ('grnors', '>i2'),
+    ('grnofr', '>i2'),
+    ('grnlof', '>i2'),
+    ('gaps',   '>i2'),
+    ('otrav',  '>i2'),
+    ('cdpx',   '>i4'),
+    ('cdpy',   '>i4'),
+    ('iline',  '>i4'),
+    ('xline',  '>i4'),
+    ('shnum',  '>i4'),
+    ('shsca',  '>i2'),
+    ('tval',   '>i2'),
+    ('tconst4', '>i4'),
+    ('tconst2', '>i2'),
+    ('tunits', '>i2'),
+    ('device', '>i2'),
+    ('tscalar', '>i2'),
+    ('stype',  '>i2'),
+    ('sendir', '>i4'),
+    ('unknown', '>i2'),
+    ('smeas4', '>i4'),
+    ('smeas2', '>i2'),
+    ('smeasu', '>i2'),
+    ('unass1', '>i4'),
+    ('unass2', '>i4')
 ])
 
 su_header_dtype = np.dtype([
@@ -332,142 +428,6 @@ su_header_dtype = np.dtype([
 ])
 
 
-segy_trace_header_dtype = np.dtype([
-    ('tracl',  '>i4'),
-    ('tracr',  '>i4'),
-    ('fldr',   '>i4'),
-    ('tracf',  '>i4'),
-    ('ep',     '>i4'),
-    ('cdp',    '>i4'),
-    ('cdpt',   '>i4'),
-    ('trid',   '>i2'),
-    ('nvs',    '>i2'),
-    ('nhs',    '>i2'),
-    ('duse',   '>i2'),
-    ('offset', '>i4'),
-    ('gelev',  '>i4'),
-    ('selev',  '>i4'),
-    ('sdepth', '>i4'),
-    ('gdel',   '>i4'),
-    ('sdel',   '>i4'),
-    ('swdep',  '>i4'),
-    ('gwdep',  '>i4'),
-    ('scalel', '>i2'),
-    ('scalco', '>i2'),
-    ('sx',     '>i4'),
-    ('sy',     '>i4'),
-    ('gx',     '>i4'),
-    ('gy',     '>i4'),
-    ('counit', '>i2'),
-    ('wevel',   '>i2'),
-    ('swevel',  '>i2'),
-    ('sut',     '>i2'),
-    ('gut',     '>i2'),
-    ('sstat',   '>i2'),
-    ('gstat',   '>i2'),
-    ('tstat',   '>i2'),
-    ('laga',    '>i2'),
-    ('lagb',    '>i2'),
-    ('delrt',   '>i2'),
-    ('muts',    '>i2'),
-    ('mute',    '>i2'),
-    ('ns',      '>i2'),
-    ('dt',      '>i2'),
-    ('gain',    '>i2'),
-    ('igc',    '>i2'),
-    ('igi',    '>i2'),
-    ('corr',   '>i2'),
-    ('sfs',    '>i2'),
-    ('sfe',    '>i2'),
-    ('slen',   '>i2'),
-    ('styp',   '>i2'),
-    ('stas',   '>i2'),
-    ('stae',   '>i2'),
-    ('tatyp',  '>i2'),
-    ('afilf',  '>i2'),
-    ('afils',  '>i2'),
-    ('nofilf', '>i2'),
-    ('nofils', '>i2'),
-    ('lcf',    '>i2'),
-    ('hcf',    '>i2'),
-    ('lcs',    '>i2'),
-    ('hcs',    '>i2'),
-    ('year',   '>i2'),
-    ('day',    '>i2'),
-    ('hour',   '>i2'),
-    ('minute', '>i2'),
-    ('sec',    '>i2'),
-    ('timbas', '>i2'),
-    ('trwf',   '>i2'),
-    ('grnors', '>i2'),
-    ('grnofr', '>i2'),
-    ('grnlof', '>i2'),
-    ('gaps',   '>i2'),
-    ('otrav',  '>i2'),
-    ('cdpx',   '>i4'),
-    ('cdpy',   '>i4'),
-    ('iline',  '>i4'),
-    ('xline',  '>i4'),
-    ('shnum',  '>i4'),
-    ('shsca',  '>i2'),
-    ('tval',   '>i2'),
-    ('tconst4', '>i4'),
-    ('tconst2', '>i2'),
-    ('tunits', '>i2'),
-    ('device', '>i2'),
-    ('tscalar', '>i2'),
-    ('stype',  '>i2'),
-    ('sendir', '>i4'),
-    ('unknown', '>i2'),
-    ('smeas4', '>i4'),
-    ('smeas2', '>i2'),
-    ('smeasu', '>i2'),
-    ('unass1', '>i4'),
-    ('unass2', '>i4')
-])
-
-
-class segy_file_header(tb.IsDescription):
-    TFH1 = tb.StringCol(80)
-    TFH2 = tb.StringCol(80)
-    TFH3 = tb.StringCol(80)
-    TFH4 = tb.StringCol(80)
-    TFH5 = tb.StringCol(80)
-    TFH6 = tb.StringCol(80)
-    TFH7 = tb.StringCol(80)
-    TFH8 = tb.StringCol(80)
-    TFH9 = tb.StringCol(80)
-    TFH10 = tb.StringCol(80)
-    TFH11 = tb.StringCol(80)
-    TFH12 = tb.StringCol(80)
-    TFH13 = tb.StringCol(80)
-    TFH14 = tb.StringCol(80)
-    TFH15 = tb.StringCol(80)
-    TFH16 = tb.StringCol(80)
-    TFH17 = tb.StringCol(80)
-    TFH18 = tb.StringCol(80)
-    TFH19 = tb.StringCol(80)
-    TFH20 = tb.StringCol(80)
-    TFH21 = tb.StringCol(80)
-    TFH22 = tb.StringCol(80)
-    TFH23 = tb.StringCol(80)
-    TFH24 = tb.StringCol(80)
-    TFH25 = tb.StringCol(80)
-    TFH26 = tb.StringCol(80)
-    TFH27 = tb.StringCol(80)
-    TFH28 = tb.StringCol(80)
-    TFH29 = tb.StringCol(80)
-    TFH30 = tb.StringCol(80)
-    TFH31 = tb.StringCol(80)
-    TFH32 = tb.StringCol(80)
-    TFH33 = tb.StringCol(80)
-    TFH34 = tb.StringCol(80)
-    TFH35 = tb.StringCol(80)
-    TFH36 = tb.StringCol(80)
-    TFH37 = tb.StringCol(80)
-    TFH38 = tb.StringCol(80)
-    TFH39 = tb.StringCol(80)
-    TFH40 = tb.StringCol(80)
 
 segy_textual_file_header = np.dtype([
     ('TFH1', '<a80'),
@@ -512,40 +472,6 @@ segy_textual_file_header = np.dtype([
     ('TFH40', (np.str_,   80)),
 ])
 
-segy_binary_file_header = np.dtype([
-    ('jobid', 'i4'),   # job identification number
-    ('lino', 'i4'),    # line number (only one line per reel)
-    ('reno', 'i4'),    # reel number
-    ('ntrpr', 'u2'),   # number of data traces per record
-    ('nart', 'u2'),    # number of auxiliary traces per record
-    ('hdt', 'u2'),     # sample interval in micro secs for this reel
-    ('dto', 'u2'),     # same for original field recording
-    ('hns', 'u2'),     # number of samples per trace for this reel
-    ('nso', 'u2'),     # same for original field recording
-    ('format', 'i2'),  # data sample format code
-    ('fold', 'i2'),    # CDP fold expected per CDP ensemble
-    ('tsort', 'i2'),   # trace sorting code
-    ('vscode', 'i2'),  # needs label
-    ('hsfs', 'i2'),    # needs label
-    ('hsfe', 'i2'),    # needs label
-    ('hslen', 'i2'),   # needs label
-    ('hstyp', 'i2'),   # needs label
-    ('schn', 'i2'),    # needs label
-    ('hstas', 'i2'),   # needs label
-    ('hstae', 'i2'),   # needs label
-    ('htatyp', 'i2'),  # needs label
-    ('hcorr', 'i2'),   # needs label
-    ('bgrcv', 'i2'),   # needs label
-    ('rcvm', 'i2'),    # needs label
-    ('mfeet', 'i2'),   # needs label
-    ('polyt', 'i2'),   # needs label
-    ('vpol', 'i2'),    # needs label
-    ('pad', 'S240'),   # needs label
-    ('revno', 'i2'),   # needs label
-    ('fxtrl', 'i2'),   # needs label
-    ('next', 'i2'),    # needs label
-    ('endpad', 'S94'), # needs label
-])
 
 if __name__ == '__main__':
     print segy_binary_file_header
