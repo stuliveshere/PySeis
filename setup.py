df56c8541c48543cc36e6e068a751763e0958e30
Stu Fletcher <mail@stuliveshere.com>
2013-11-06 21:44:14 +1000

    updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='PySeis',
+      version='0.1',
+      description='Seismic Processing Package'
+      url='http://github.com/stuliveshere/PySeis',
+      author='Stewart Fletcher',
+      author_email='mail@stuliveshere.com',
+      license='MIT',
+      packages=['PySeis'],
+      zip_safe=False)
