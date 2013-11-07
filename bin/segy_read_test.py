29384ab112ccd2ec90c254758d1fef072d6e8861
Stu Fletcher <mail@stuliveshere.com>
2013-11-07 13:16:47 +1000

    testing ibm float converter

--- bin/segy_read_test.py
+++ bin/segy_read_test.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python2.7
 import  PySeis.segy as d
+import PySeis.misc as c
+import PySeis.su as su
 import numpy as np
+import matplotlib.pyplot as pylab
 
 file = open('../data/sample.sgy', 'rb')
 
@@ -19,3 +22,22 @@ print bh['format']
 #need to figure out how to efficiently decode large quantifies of  ibm 
 #floats in files. may need ctypes?
 
+th = file.read(240)
+
+th = np.array(th, dtype=d.segy_trace_header_dtype)
+
+print th['ns']
+
+trace = file.read(2001*4)
+
+trace = np.fromstring(trace, dtype='>i4').tolist()
+
+trace = np.array([c.ibm2ieee(a) for a in trace])
+pylab.plot(trace)
+
+
+data = su.readSU('../data/sample.su')
+pylab.plot(data[0]['trace'])
+
+
+pylab.show()
