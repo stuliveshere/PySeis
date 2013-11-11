3410701631417911346084b19aa80230516fdc85
Stu Fletcher <mail@stuliveshere.com>
2013-11-11 21:47:13 +1000

    started building segy class

--- PySeis/segy/definitions.py
+++ PySeis/segy/definitions.py
@@ -1,139 +1,20 @@
-import tables as tb
 import numpy as np
 
-class segy_file_header(tb.IsDescription):
-    TFH1 = tb.StringCol(80)
-    #~ TFH2 = tb.StringCol(80)
-    #~ TFH3 = tb.StringCol(80)
-    #~ TFH4 = tb.StringCol(80)
-    #~ TFH5 = tb.StringCol(80)
-    #~ TFH6 = tb.StringCol(80)
-    #~ TFH7 = tb.StringCol(80)
-    #~ TFH8 = tb.StringCol(80)
-    #~ TFH9 = tb.StringCol(80)
-    #~ TFH10 = tb.StringCol(80)
-    #~ TFH11 = tb.StringCol(80)
-    #~ TFH12 = tb.StringCol(80)
-    #~ TFH13 = tb.StringCol(80)
-    #~ TFH14 = tb.StringCol(80)
-    #~ TFH15 = tb.StringCol(80)
-    #~ TFH16 = tb.StringCol(80)
-    #~ TFH17 = tb.StringCol(80)
-    #~ TFH18 = tb.StringCol(80)
-    #~ TFH19 = tb.StringCol(80)
-    #~ TFH20 = tb.StringCol(80)
-    #~ TFH21 = tb.StringCol(80)
-    #~ TFH22 = tb.StringCol(80)
-    #~ TFH23 = tb.StringCol(80)
-    #~ TFH24 = tb.StringCol(80)
-    #~ TFH25 = tb.StringCol(80)
-    #~ TFH26 = tb.StringCol(80)
-    #~ TFH27 = tb.StringCol(80)
-    #~ TFH28 = tb.StringCol(80)
-    #~ TFH29 = tb.StringCol(80)
-    #~ TFH30 = tb.StringCol(80)
-    #~ TFH31 = tb.StringCol(80)
-    #~ TFH32 = tb.StringCol(80)
-    #~ TFH33 = tb.StringCol(80)
-    #~ TFH34 = tb.StringCol(80)
-    #~ TFH35 = tb.StringCol(80)
-    #~ TFH36 = tb.StringCol(80)
-    #~ TFH37 = tb.StringCol(80)
-    #~ TFH38 = tb.StringCol(80)
-    #~ TFH39 = tb.StringCol(80)
-    #~ TFH40 = tb.StringCol(80)
-    
-segy_textual_header_dtype = np.dtype([
-    ('TFH1', (np.str_,   80)),
-    ('TFH2', (np.str_,   80)),
-    ('TFH3', (np.str_,   80)),
-    ('TFH4', (np.str_,   80)),
-    ('TFH5', (np.str_,   80)),
-    ('TFH6', (np.str_,   80)),
-    ('TFH7', (np.str_,   80)),
-    ('TFH8', (np.str_,   80)),
-    ('TFH9', (np.str_,   80)),
-    ('TFH10', (np.str_,   80)),
-    ('TFH11', (np.str_,   80)),
-    ('TFH12', (np.str_,   80)),
-    ('TFH13', (np.str_,   80)),
-    ('TFH14', (np.str_,   80)),
-    ('TFH15', (np.str_,   80)),
-    ('TFH16', (np.str_,   80)),
-    ('TFH17', (np.str_,   80)),
-    ('TFH18', (np.str_,   80)),
-    ('TFH19', (np.str_,   80)),
-    ('TFH20', (np.str_,   80)),
-    ('TFH21', (np.str_,   80)),
-    ('TFH22', (np.str_,   80)),
-    ('TFH23', (np.str_,   80)),
-    ('TFH24', (np.str_,   80)),
-    ('TFH25', (np.str_,   80)),
-    ('TFH26', (np.str_,   80)),
-    ('TFH27', (np.str_,   80)),
-    ('TFH28', (np.str_,   80)),
-    ('TFH29', (np.str_,   80)),
-    ('TFH30', (np.str_,   80)),
-    ('TFH31', (np.str_,   80)),
-    ('TFH32', (np.str_,   80)),
-    ('TFH33', (np.str_,   80)),
-    ('TFH34', (np.str_,   80)),
-    ('TFH35', (np.str_,   80)),
-    ('TFH36', (np.str_,   80)),
-    ('TFH37', (np.str_,   80)),
-    ('TFH38', (np.str_,   80)),
-    ('TFH39', (np.str_,   80)),
-    ('TFH40', (np.str_,   80)),
-])
-    
-class segy_binary_header(tb.IsDescription):
-    jobid = tb.Int32Col()
-    lino = tb.Int32Col()
-    reno = tb.Int32Col()
-    ntrpr = tb.Int16Col()
-    nart = tb.Int16Col()
-    hdt = tb.UInt16Col()
-    dto = tb.UInt16Col()
-    hns = tb.UInt16Col()
-    nso = tb.UInt16Col()
-    format = tb.Int16Col()
-    fold = tb.Int16Col()
-    tsort = tb.Int16Col()
-    vscode = tb.Int16Col()
-    hsfs = tb.Int16Col()
-    hsfe = tb.Int16Col()
-    hslen = tb.Int16Col()
-    hstyp = tb.Int16Col()
-    schn = tb.Int16Col()
-    hstas = tb.Int16Col()
-    hstae = tb.Int16Col()
-    htatyp = tb.Int16Col()
-    hcorr = tb.Int16Col()
-    bgrcv = tb.Int16Col()
-    rcvm = tb.Int16Col()
-    mfeet = tb.Int16Col()
-    polyv = tb.Int16Col()
-    vpol = tb.Int16Col()
-    unassigned_1 = tb.StringCol(240)
-    segyrev = tb.Int16Col()
-    fixedlen = tb.Int16Col()
-    numhdr = tb.Int16Col()
-    unassigned_2 = tb.StringCol(94)
-
+segy_textual_header_dtype = np.dtype([('TFH', (np.str_,   3200))])
 
 segy_binary_header_dtype = np.dtype([
     ('jobid', '>i4'),
     ('lino', '>i4'),
     ('reno', '>i4'),
-    ('ntrpr', '>i2'),
-    ('nart', '>i2'),
-    ('hdt', '>u2'),
+    ('ntrpr', '>i2'), # mandatory (prestack)
+    ('nart', '>i2'), # mandatory (prestack)
+    ('hdt', '>u2'), # mandatory (all)
     ('dto', '>u2'),
-    ('hns', '>u2'),
+    ('hns', '>u2'), # mandatory (all)
     ('nso', '>u2'),
-    ('format', '>i2'),
-    ('fold', '>i2'),
-    ('tsort', '>i2'),
+    ('format', '>i2'), # mandatory (all)
+    ('fold', '>i2'), # strongly recommended 
+    ('tsort', '>i2'), # strongly recommended 
     ('vscode', '>i2'),
     ('hsfs', '>i2'),
     ('hsfe', '>i2'),
@@ -146,118 +27,25 @@ segy_binary_header_dtype = np.dtype([
     ('hcorr', '>i2'),
     ('bgrcv', '>i2'),
     ('rcvm', '>i2'),
-    ('mfeet', '>i2'),
+    ('mfeet', '>i2'), # strongly recommended 
     ('polyv', '>i2'),
     ('vpol', '>i2'),
     ('unassigned_1', (np.str_,   240)),
-    ('segyrev', '>i2'),
-    ('fixedlen', '>i2'),
-    ('numhdr', '>i2'),
+    ('segyrev', '>i2'), # mandatory (all)
+    ('fixedlen', '>i2'), # mandatory (all)
+    ('numhdr', '>i2'), # mandatory (all)
     ('unassigned_2', (np.str_,   94)),
 ])
 
-class segy_trace_header(tb.IsDescription):
-    tracl =  tb.Int32Col()
-    tracr =  tb.Int32Col()
-    fldr =   tb.Int32Col()
-    tracf =  tb.Int32Col()
-    ep =     tb.Int32Col()
-    cdp =    tb.Int32Col()
-    cdpt =   tb.Int32Col()
-    trid =   tb.Int32Col()
-    nvs =    tb.Int32Col()
-    nhs =    tb.Int32Col()
-    duse =   tb.Int32Col()
-    offset = tb.Int32Col()
-    gelev =  tb.Int32Col()
-    selev =  tb.Int32Col()
-    sdepth = tb.Int32Col()
-    gdel =   tb.Int32Col()
-    sdel =   tb.Int32Col()
-    swdep =  tb.Int32Col()
-    gwdep =  tb.Int32Col()
-    scalel = tb.Int32Col()
-    scalco = tb.Int32Col()
-    sx =     tb.Int32Col()
-    sy =     tb.Int32Col()
-    gx =     tb.Int32Col()
-    gy =     tb.Int32Col()
-    counit = tb.Int32Col()
-    wevel =   tb.Int32Col()
-    swevel =  tb.Int32Col()
-    sut =     tb.Int32Col()
-    gut =     tb.Int32Col()
-    sstat =   tb.Int32Col()
-    gstat =   tb.Int32Col()
-    tstat =   tb.Int32Col()
-    laga =    tb.Int32Col()
-    lagb =    tb.Int32Col()
-    delrt =   tb.Int32Col()
-    muts =    tb.Int32Col()
-    mute =    tb.Int32Col()
-    ns =      tb.Int32Col()
-    dt =      tb.Int32Col()
-    gain =    tb.Int32Col()
-    igc =    tb.Int32Col()
-    igi =    tb.Int32Col()
-    corr =   tb.Int32Col()
-    sfs =    tb.Int32Col()
-    sfe =    tb.Int32Col()
-    slen =   tb.Int32Col()
-    styp =   tb.Int32Col()
-    stas =   tb.Int32Col()
-    stae =   tb.Int32Col()
-    tatyp =  tb.Int32Col()
-    afilf =  tb.Int32Col()
-    afils =  tb.Int32Col()
-    nofilf = tb.Int32Col()
-    nofils = tb.Int32Col()
-    lcf =    tb.Int32Col()
-    hcf =    tb.Int32Col()
-    lcs =    tb.Int32Col()
-    hcs =    tb.Int32Col()
-    year =   tb.Int32Col()
-    day =    tb.Int32Col()
-    hour =   tb.Int32Col()
-    minute = tb.Int32Col()
-    sec =    tb.Int32Col()
-    timbas = tb.Int32Col()
-    trwf =   tb.Int32Col()
-    grnors = tb.Int32Col()
-    grnofr = tb.Int32Col()
-    grnlof = tb.Int32Col()
-    gaps =   tb.Int32Col()
-    otrav =  tb.Int32Col()
-    cdpx =   tb.Int32Col()
-    cdpy =   tb.Int32Col()
-    iline =  tb.Int32Col()
-    xline =  tb.Int32Col()
-    shnum =  tb.Int32Col()
-    shsca =  tb.Int32Col()
-    tval =   tb.Int32Col()
-    tconst4 = tb.Int32Col()
-    tconst2 = tb.Int32Col()
-    tunits = tb.Int32Col()
-    device = tb.Int32Col()
-    tscalar = tb.Int32Col()
-    stype =  tb.Int32Col()
-    sendir = tb.Int32Col()
-    unknown = tb.Int32Col()
-    smeas4 = tb.Int32Col()
-    smeas2 = tb.Int32Col()
-    smeasu = tb.Int32Col()
-    unass1 = tb.Int32Col()
-    unass2 = tb.Int32Col()
-
 segy_trace_header_dtype = np.dtype([
-    ('tracl',  '>i4'),
+    ('tracl',  '>i4'), # strongly recommended 
     ('tracr',  '>i4'),
-    ('fldr',   '>i4'),
-    ('tracf',  '>i4'),
+    ('fldr',   '>i4'), # strongly recommended 
+    ('tracf',  '>i4'), # strongly recommended 
     ('ep',     '>i4'),
     ('cdp',    '>i4'),
     ('cdpt',   '>i4'),
-    ('trid',   '>i2'),
+    ('trid',   '>i2'), # strongly recommended 
     ('nvs',    '>i2'),
     ('nhs',    '>i2'),
     ('duse',   '>i2'),
@@ -288,8 +76,8 @@ segy_trace_header_dtype = np.dtype([
     ('delrt',   '>i2'),
     ('muts',    '>i2'),
     ('mute',    '>i2'),
-    ('ns',      '>i2'),
-    ('dt',      '>i2'),
+    ('ns',      '>i2'), # strongly recommended 
+    ('dt',      '>i2'), # strongly recommended 
     ('gain',    '>i2'),
     ('igc',    '>i2'),
     ('igi',    '>i2'),
@@ -343,3 +131,70 @@ segy_trace_header_dtype = np.dtype([
     ('unass2', '>i4')
 ])
 
+keylist = 	segy_textual_header_dtype.names + \
+		segy_binary_header_dtype.names + \
+		segy_trace_header_dtype.names 
+
+class segy:
+	def __init__(self, filename=None, **kwargs):
+		if not set(kwargs.keys()).issubset(keylist):
+			raise Exception("Invalid key in segy kwargs")
+		if not filename: 
+			self.initialiseFramework()
+		else:
+			self.loadFramework()
+			
+	def initialiseFramework(self):
+		pass
+		#initialise text header
+		
+		#initialise binary header
+	
+	def loadFramework(self):
+		pass
+		#pull a few key items in from segy file
+		
+	def importFIle(self, h5):
+		pass
+		
+	def exportFIle(self, filename):
+		pass
+	
+	def ibm2ieee(ibm): 
+	    s = ibm >> 31 & 0x01 
+	    exp = ibm >> 24 & 0x7f 
+	    fraction = (ibm & 0x00ffffff).astype(np.float32) / 16777216.0
+	    ieee = (1.0 - 2.0 * s) * fraction * np.power(np.float32(16.0), exp - 64.0) 
+	    return ieee 
+
+	def ieee2ibm(ieee): 
+	    ieee = ieee.astype(np.float32) 
+	    expmask = 0x7f800000 
+	    signmask = 0x80000000 
+	    mantmask = 0x7fffff 
+	    asint = ieee.view('i4') 
+	    signbit = asint & signmask 
+	    exponent = ((asint & expmask) >> 23) - 127 
+	    # The IBM 7-bit exponent is to the base 16 and the mantissa is presumed to 
+	    # be entirely to the right of the radix point. In contrast, the IEEE 
+	    # exponent is to the base 2 and there is an assumed 1-bit to the left of the 
+	    # radix point. 
+	    exp16 = ((exponent+1) // 4) 
+	    exp_remainder = (exponent+1) % 4 
+	    exp16 += exp_remainder != 0 
+	    downshift = np.where(exp_remainder, 4-exp_remainder, 0) 
+	    ibm_exponent = np.clip(exp16 + 64, 0, 127) 
+	    expbits = ibm_exponent << 24 
+	    # Add the implicit initial 1-bit to the 23-bit IEEE mantissa to get the 
+	    # 24-bit IBM mantissa. Downshift it by the remainder from the exponent's 
+	    # division by 4. It is allowed to have up to 3 leading 0s. 
+	    ibm_mantissa = ((asint & mantmask) | 0x800000) >> downshift 
+	    # Special-case 0.0 
+	    ibm_mantissa = np.where(ieee, ibm_mantissa, 0) 
+	    expbits = np.where(ieee, expbits, 0) 
+	    return signbit | expbits | ibm_mantissa
+
+		
+if __name__ == '__main__':
+	a = segy()
+	
--- bin/segy_read_test.py
+++ bin/segy_read_test.py
@@ -8,22 +8,22 @@ import tables as tb
 import time
 
 
-#open stuff 
-#~ file = '../data/sample.sgy'
+#~ #open stuff 
+input = '../data/sample.sgy'
 
 
-#initialise database
+#~ #initialise database
 h5file = tb.openFile("../data/test1.h5", mode = "w", title = "Test file")
 line1 = h5file.createGroup("/", 'line1', 'Line 1 Survey')
-file_header = h5file.createTable(line1, 'FH', d.segy_file_header, "EBCDIC File Header")
+file_header = h5file.createTable(line1, 'FH', d.segy_textual_header_dtype, "EBCDIC File Header")
 binary_header = h5file.createTable(line1, 'BH', d.segy_binary_header_dtype, "Binary File Header")
 trace_header = h5file.createTable(line1, 'TH', d.segy_trace_header_dtype, "Trace Header")
 
-#we really need to reach in and find the number of samples so we can initialise the trace dataset
-#first ns will be 40*80 + 400 + 114 = 3714
-#or i could be lazy, read the whole of the first trace header and lift out ns
+#~ #we really need to reach in and find the number of samples so we can initialise the trace dataset
+#~ #first ns will be 40*80 + 400 + 114 = 3714
+#~ #or i could be lazy, read the whole of the first trace header and lift out ns
 
-with open(file, 'rb') as f:
+with open(input, 'rb') as f:
 	f.seek(3600)
 	ns = np.array(f.read(240), dtype=d.segy_trace_header_dtype)['ns']
 	
@@ -32,14 +32,14 @@ trace_data = h5file.create_earray(     line1,
 							atom = tb.Float32Atom(), 
 							shape = (0,ns),
 							title = "Trace Header", 
-							filters = tb.Filters(complevel=1, complib='lzo'))
+							filters = tb.Filters(complevel=1, complib='zlib'))
 							
 
 
-with open(file, 'rb') as f:
+with open(input, 'rb') as f:
 	#read in EBCDIC header and write to table
 	fh = f.read(3200).decode('EBCDIC-CP-BE').encode('ascii')
-	file_header.append(np.fromstring(fh, dtype='S80'))
+	file_header.append(np.fromstring(fh, dtype=d.segy_textual_header_dtype))
 	
 	#repeat for binary header
 	bh = f.read(400)	
@@ -57,6 +57,6 @@ with open(file, 'rb') as f:
 h5file.flush()
 
 	
-	
+#~ a = d.segy()
 
 
