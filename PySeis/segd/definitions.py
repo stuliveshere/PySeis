4f8c363bdae22c9516693c24cb66b36d09a15e4c
Stu Fletcher <mail@stuliveshere.com>
2013-12-10 01:36:38 +1000

    segd experiments

--- PySeis/segd/definitions.py
+++ PySeis/segd/definitions.py
@@ -5,7 +5,7 @@ segd_general_header = np.dtype([
 ('format', 'S4'), #format code  
 ('constants', 'S12'), #general constants
 ('year', 'S2'), #last 2 digits of year
-('GH', 'S1'), #number of additional blocks
+('GH', 'uint8'), #number of additional blocks
 ('julian', 'S3'), #julian day
 ('hour','S2'), #hour (UTC)
 ('min', 'S2'), #minute
@@ -17,10 +17,37 @@ segd_general_header = np.dtype([
 ('polarity', 'S1'), #record polarity
 ('null2',  'S3'),
 ('type', 'S1'), #record type
-('len', 'S3'), #record length
+('len1', 'uint16'), #record length
+('len2', 'uint16'), #record length
+('len3', 'uint16'), #record length
 ('STR', 'S2'),
 ('CS', 'S2'),
 ('SK', 'S2'),
 ('EC', 'S2'),
 ('EX', 'S2'),
 ])
+
+segd_general_header_list =[
+('fileno', 4), #file number
+('format', 4), #format code  
+('constants', 12), #general constants
+('year', 2), #last 2 digits of year
+('GH', 1), #number of additional blocks
+('julian',3), #julian day
+('hour',2), #hour (UTC)
+('min', 2), #minute
+('sec',2), #second
+('mcode', 2), #manufacturer's code
+('mserial', 4), #manufacturer's serial
+('null', 6), #not used
+('bscan', 2), #base scan interval
+('polarity', 1), #record polarity
+('null2',  3),
+('type', 1), #record type
+('len', 3), #record length
+('STR', 2),
+('CS', 2),
+('SK', 2),
+('EC', 2),
+('EX', 2),
+]
--- bin/segd_read_test.py
+++ bin/segd_read_test.py
@@ -7,18 +7,29 @@ def bcd(uints):
 	c = np.unpackbits(uints).reshape(-1,4)
 	d = np.zeros_like(c)
 	e =  np.hstack((d, c))
-	f = np.packbits(e)
-	f[f == 15] = 0
-	return ''.join(f.astype(np.str).tolist())
+	f = np.packbits(e).astype(np.uint8)
+	return f
 	
+def bcd2(uints):
+	n = uints & 0xf
+	m =  (uints  >> 4) & 0xf
+	return np.dstack([m, n]).flatten()	
 
 a = open(file, 'r').read(32)
 b = np.fromstring(a, dtype=np.uint8)
-c = bcd(b)
+c = bcd(b).astype('S').tolist()
+print c
+r = bcd2(b)
 
-print np.array(c, dtype = d.segd_general_header)
+s = d.segd_general_header_list
+header = {}
 
-
+for key, value in s:
+	x =  c[:value]
+	c = c[value:]
+	header[key] = ''.join(x)
+print header
+	
 
 
 
