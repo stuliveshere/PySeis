4eb29e384c6b75e185699e6d0f7ed5ccb4b93afa
Stu Fletcher <mail@stuliveshere.com>
2013-12-30 21:25:19 +1000

    timing

--- bin/pytables_experiments/code/brute/find_dups.py
+++ bin/pytables_experiments/code/brute/find_dups.py
@@ -1,7 +1,9 @@
+from __future__ import print_function #python 3
 import tables as tb
 import cProfile
 import time
 import numpy as np
+
 
 class Timer:    
     def __enter__(self):
@@ -20,14 +22,14 @@ def dups1(th):
 			query = '(id == %d) & (counts == %d)' %(id, counts)
 			result = th.readWhere(query)
 			if len(result) > 1:
-				print row['id'], row['counts']
-	print '''
+				print(row['id'], row['counts'])
+	print('''
 	brute force method
 	with row looksups
 	rectangular search
 	nrows=%d
 	time = %.3fs
-	''' 	%(th.nrows, t.interval)
+	''') %(th.nrows, t.interval)
 		
 		
 def dups2(th):
@@ -38,15 +40,15 @@ def dups2(th):
 			query = '(id == %d) & (counts == %d)' %(id, counts)
 			result = th.readWhere(query, start=row.nrow, stop=th.nrows)
 			if len(result) > 1:
-				print row['id']
-	print '''
+				print(row['id'])
+	print('''
 	brute force method
 	triangular search
 	row lookups
 	nrows=%d
 	time = %.3fs	
 	
-	''' %(th.nrows, t.interval)
+	''') %(th.nrows, t.interval)
 
 
 def dups3(th):
@@ -54,30 +56,30 @@ def dups3(th):
 		for row in th:
 			result = th.readWhere('(id == cid) & (counts == ccounts)', {'cid': row['id'], 'ccounts': row['counts']})
 			if len(result) > 1:
-				print row['id']
-	print '''
+				print(row['id'])
+	print('''
 	brute force method
 	rectangular search
 	with condvals
 	nrows=%d
 	time = %.3fs	
 	
-	'''%(th.nrows, t.interval)
+	''')%(th.nrows, t.interval)
 			
 def dups4(th):
 	with Timer() as t:
 		for row in th:
 			result = th.readWhere('(id == cid) & (counts == ccounts)', {'cid': row['id'], 'ccounts': row['counts']}, start=row.nrow, stop=th.nrows)
 			if len(result) > 1:
-				print row['id']	
-	print '''
+				print(row['id'])
+	print('''
 	brute force method
 	triangular search
 	with condvals
 	nrows=%d
 	time = %.3fs
 	
-	'''%(th.nrows, t.interval)
+	''')%(th.nrows, t.interval)
 
 			
 def dups5(th):
@@ -88,43 +90,47 @@ def dups5(th):
 	with Timer() as t:
 		for row in th:
 			result = th.where('(id == cid) & (counts == ccounts)', {'cid': row['id'], 'ccounts': row['counts']})
-			print dir(result._iter)
-			print result._iter
-						
-			break
-			for row in result:
-				print row
-			print ''
-			print ''
-	print '''
+	print( '''
 	brute force method
 	iterator only
 	with condvals
 	nrows=%d
 	time = %.3fs	
 	
-	'''%(th.nrows, t.interval)
+	''')%(th.nrows, t.interval)
 
 def dups6(th):
 	'''
 	bloom filter
 	'''
-
 	with Timer() as t:
 		x = th.cols.id
 		y = th.cols.counts
-		r = np.zeros(th.nrows, dtype=np.int32)
-		expr = '(x + (y *7919)) % 4523'
+		#~ r = np.zeros(th.nrows, dtype=np.int32)
+		expr = '(x * 65536) + y'
 		ex = tb.Expr(expr)
-		ex.setOutput(r)
+		ex.setOutput(th.cols.hash)
 		ex.eval()
-		z = th.itersorted(r)
-	print '''
+		indexrows = th.cols.hash.create_csindex(filters=filters)
+		z = th.itersorted(sortby=th.cols.hash)
+		ref = None
+		for row in z:
+			if row['hash'] == ref:
+				hash = row['hash'] 
+				count = hash% 65536
+				id = int((hash - count) / 65536)
+				#~ print(id, count)
+				#~ print(row['id'], row['counts'])
+				#~ print()
+			ref = row['hash']
+
+		
+	print('''
 	hash calculation
 	nrows=%d
 	time = %.3fs	
 	
-	'''%(th.nrows, t.interval)	
+	''' %(th.nrows, t.interval))
 	
 def dups7(th):
 	'''
@@ -136,15 +142,15 @@ def dups7(th):
 		for row in z:
 			result = th.readWhere('(id == cid) & (counts == ccounts)', {'cid': row['id'], 'ccounts': row['counts']}, start=row.nrow, stop=row.nrow+1)
 			if len(result) > 1:
-				print row['id']	
-	print '''
+				print(row['id'])
+	print('''
 	sort method
 	adjacent search
 	with condvals
 	nrows=%d
 	time = %.3fs
 	
-	'''%(th.nrows, t.interval)
+	''')%(th.nrows, t.interval)
 	
 
 def dups8(th):
@@ -157,17 +163,17 @@ def dups8(th):
 		counts = None
 		for row in z:
 			if (row['id'] == id) & (row['counts'] == counts) :
-				print row['id'], row['counts']
+				print(row['id'], row['counts'])
 			id = row['id']
 			counts = row['counts']
 				
-	print '''
+	print('''
 	sort method
 	simple adjacent search
 	nrows=%d
 	time = %.3fs
 	
-	'''%(th.nrows, t.interval)		
+	''')%(th.nrows, t.interval)		
 	
 def dups9(th):
 	''' 
@@ -179,17 +185,17 @@ def dups9(th):
 			iters.append(th.get_where_list('(id == cid) & (counts == ccounts)', {'cid': row['id'], 'ccounts': row['counts']}))
 		for i in iters:
 			if len(i) > 1:
-				print th[i[1]]['id']
+				print(th[i[1]]['id'])
 			
 		
-	print '''
+	print('''
 	brute force method
 	iterator only
 	with condvals
 	nrows=%d
 	time = %.3fs	
 	
-	'''%(th.nrows, t.interval)	
+	''')%(th.nrows, t.interval)	
 
 
 if __name__ == '__main__':
@@ -198,10 +204,10 @@ if __name__ == '__main__':
 		nrows = int(10**n)
 		db = tb.openFile('db.h5', mode = "w", title='test')
 		node = db.createGroup("/", 'line', 'Node')
-		mytype = np.dtype([('id', np.int32),('counts', np.int32)])
+		mytype = np.dtype([('id', np.int64),('counts', np.int64), ('hash', np.int64)])
 		filters = tb.Filters(complib='blosc', complevel=1)
 		th = db.createTable(node, 'TH', mytype, "Headers", expectedrows=nrows, filters=filters)
-		data = np.random.randint(1, 2**12, (nrows, 2)).astype(np.int32)
+		data = np.random.randint(1, 2**16, (nrows, 3)).astype(np.int64)
 		th.append(data)
 		indexrows = th.cols.id.create_csindex(filters=filters)
 		indexrows = th.cols.counts.create_csindex(filters=filters)	
@@ -210,8 +216,8 @@ if __name__ == '__main__':
 		#~ dups2(th)
 		#~ dups3(th)
 		#~ dups4(th)
-		dups5(th)
-		#~ dups6(th)
+		#~ dups5(th)
+		dups6(th)
 		#~ dups7(th)
 		#~ dups8(th)
 		#~ dups9(th)
