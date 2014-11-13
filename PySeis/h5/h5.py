bc7feccf0007e800d3b07f279aaf9d29318a9a04
Stu Fletcher <mail@stuliveshere.com>
2014-11-13 14:31:27 +1000

    playing with h5

--- PySeis/h5/h5.py
+++ PySeis/h5/h5.py
@@ -1,8 +1,121 @@
 '''
-initialises an existing h5 database for use with pyseis
+raw segy import into database
 '''
 import tables as tb
+from tables.nodes import filenode
+import os
+import warnings
+warnings.filterwarnings('ignore', category=tb.NaturalNameWarning)
+import numpy as np
+import sys
 
-class pyseis:
-    def __init__(self, database):
-        self.db = tb.openFile(database, mode = "r", title='test')
+filter_ = tb.Filters(complib='zlib', complevel=1)
+
+tfh = np.dtype([('TFH', (np.str_,  80))])
+
+bfh = np.dtype([
+    ('jobid', 'i4'),
+    ('lino', 'i4'),
+    ('reno', 'i4'),
+    ('ntrpr', 'i2'), # mandatory (prestack)
+    ('nart', 'i2'), # mandatory (prestack)
+    ('hdt', 'u2'), # mandatory (all)
+    ('dto', 'u2'),
+    ('hns', 'u2'), # mandatory (all)
+    ('nso', 'u2'),
+    ('format', 'i2'), # mandatory (all)
+    ('fold', 'i2'), # strongly recommended 
+    ('tsort', 'i2'), # strongly recommended 
+    ('vscode', 'i2'),
+    ('hsfs', 'i2'),
+    ('hsfe', 'i2'),
+    ('hslen', 'i2'),
+    ('hstyp', 'i2'),
+    ('schn', 'i2'),
+    ('hstas', 'i2'),
+    ('hstae', 'i2'),
+    ('htatyp', 'i2'),
+    ('hcorr', 'i2'),
+    ('bgrcv', 'i2'),
+    ('rcvm', 'i2'),
+    ('mfeet', 'i2'), # strongly recommended 
+    ('polyv', 'i2'),
+    ('vpol', 'i2'),
+    ('unassigned_1', (np.str_,   240)),
+    ('segyrev', 'i2'), # mandatory (all)
+    ('fixedlen', 'i2'), # mandatory (all)
+    ('numhdr', 'i2'), # mandatory (all)
+    ('unassigned_2', (np.str_,   94)),
+])
+
+def init(database):  
+    db = tb.open_file(database, 'w', title='PySeis')
+    db.createGroup(db.root, 'raws', 'raws')
+    db.createGroup(db.root.raws, 'segy', 'segy')
+    db.createGroup(db.root.raws, 'segd', 'segd')
+    db.createGroup(db.root.raws, 'su', 'su')
+    return db
+
+def add_raw(db, source, dest):
+    fsize = os.path.getsize(source)
+
+    fnode = filenode.new_node(db, where=db.root.raws.segy.files, name=dest, expectedsize=fsize, filters=filter_)
+    fnode.write(open(source).read())
+    fnode.attrs.tape_format="segy"
+    fnode.close()
+    
+def extract_EBCDIC(leaf):
+    ''''function to read EBCDIC header'''
+    data = leaf[:3200].tostring()
+    ascii = data.decode('EBCDIC-CP-BE').encode('ascii')
+    return np.fromstring(ascii, dtype=tfh)
+ 
+def extract_BFH(leaf):
+    '''assumes format header is set'''
+    data =np.frombuffer(leaf[3200:3200+400], dtype=bfh)
+    try:
+        assert 0 < data['format'] < 9
+        return data
+    except AssertionError:
+        return data.byteswap()
+        
+def clear_file(leaf):
+    '''removes table data while preserving metadata.
+    eventually this will allow processing history/
+    parameters to be stored and will give the option
+    of re-creating the dataset'''
+    leaf.truncate(0)
+
+if __name__ == '__main__':
+    os.chdir('../../data/')
+    path = '/coalStor/University_QLD/crystal_mountain/2014/data/segy/'
+    filelist = [a for a in os.listdir(path) if '.segy' in a]
+    a = init('segy_test.h5')
+    a.createGroup(a.root.raws.segy, 'files', 'segy files')
+    for file_ in filelist:
+        add_raw(a, path+file_, file_)
+    
+    a.createGroup(a.root.raws.segy, 'EBCDIC', 'textual file header')
+    a.createGroup(a.root.raws.segy, 'BFH', 'binary file header') 
+    for leaf in a.root.raws.segy.files._f_iter_nodes(classname='Leaf'):
+        name =  leaf.name
+        th = extract_EBCDIC(leaf)
+        a.create_table(a.root.raws.segy.EBCDIC, name='%s.EBCDIC' %name, description=th, filters=filter_, expectedrows=40)
+        bh = extract_BFH(leaf)
+        a.create_table(a.root.raws.segy.BFH, name='%s.BFH' %name, description=bh, filters=filter_, expectedrows=1)
+ 
+
+     
+     
+     
+
+     
+     
+     
+
+     
+     
+     
+     
+     
+
--- PySeis/segy/definitions.py
+++ PySeis/segy/definitions.py
@@ -141,100 +141,96 @@ segy_trace_header_dtype = np.dtype([
 
 
 class segy:
-    def __init__(self, database, **kwargs):
-        #~ if not set(kwargs.keys()).issubset(keylist):
-            #~ raise Exception("Invalid key in segy kwargs")
-        #~ if not filename: 
-            #~ self.initialiseFramework()
-        #~ else:
-            #~ self.loadFramework()
-        self.db = tb.openFile(database, mode = "w", title='test')
+    def __init__(self):
+        pass
         
     def read(self, filelist):
-        def read_EBCDIC(file_):
-            ''''function to read EBCDIC header'''
-            with open(file_, 'rb') as f:
-                header = np.fromfile(f, dtype='u2', count=3200/2)
-                if np.any(np.diff(header)):
-                    f.seek(0)
-                    return f.read(3200).decode('EBCDIC-CP-BE').encode('ascii')
-                else:
-                    return None
-
-        def read_bheader(file_):
-            ''''function to read binary header'''
-            with open(file_, 'rb') as f:
-                f.seek(3200)
-                binary = np.fromstring(f.read(400), dtype=segy_binary_header_dtype)
-                #endian sanity checks. this is pretty crude and will need revisting.
-                try:
-                    assert 0 < binary['format'] < 9
-                except AssertionError:
-                    binary = binary.byteswap()
-                return binary
-
-        def num_traces(file_, ns):
-                with open(file_, 'rb') as f:
-                    f.seek(0, os.SEEK_END)
-                    size = f.tell()
-                    nt = (size-3600.0)/(240.0+ns*4.0)
-                    assert nt % 1 == 0
-                return nt
-
-        def ibm2ieee(ibm):
-            s = ibm >> 31 & 0x01 
-            exp = ibm >> 24 & 0x7f 
-            fraction = (ibm & 0x00ffffff).astype(np.float32) / 16777216.0
-            ieee = (1.0 - 2.0 * s) * fraction * np.power(np.float32(16.0), exp - 64.0) 
-            return ieee
+        pass
         
         
-        node = self.db.createGroup("/", 'jobname', 'PySeis Node')
-        fh = self.db.createTable(node, 'FH', segy_textual_header_dtype, "EBCDIC File Header")
-        bh = self.db.createTable(node, 'BH', segy_binary_header_dtype, "Binary File Header")
-        for file_ in filelist:
-            #print file_
-            EBCDIC = read_EBCDIC(file_)
-            if EBCDIC: fh.append(EBCDIC)
-            bheader = read_bheader(file_)
-            #bheader['ntrpr'] = num_traces(file_, bheader['hns']) - bheader['nart']
-            bh.append(bheader)
-            self.db.flush()
-            
-        assert np.any(np.diff(bh.cols.hns[:])) == False #check ns is constant
-        assert np.any(np.diff(bh.cols.hdt[:])) == False #check ns is constant
-        ns = bh.cols.hns[0]
-        dt = bh.cols.hdt[0]
+    def read_EBCDIC(self):
+        ''''function to read EBCDIC header'''
+        with open(file_, 'rb') as f:
+            header = np.fromfile(f, dtype='u2', count=3200/2)
+            if np.any(np.diff(header)):
+                f.seek(0)
+                return f.read(3200).decode('EBCDIC-CP-BE').encode('ascii')
+            else:
+                return None
+
+        #~ def read_bheader(file_):
+            #~ ''''function to read binary header'''
+            #~ with open(file_, 'rb') as f:
+                #~ f.seek(3200)
+                #~ binary = np.fromstring(f.read(400), dtype=segy_binary_header_dtype)
+                #~ #endian sanity checks. this is pretty crude and will need revisting.
+                #~ try:
+                    #~ assert 0 < binary['format'] < 9
+                #~ except AssertionError:
+                    #~ binary = binary.byteswap()
+                #~ return binary
+
+        #~ def num_traces(file_, ns):
+                #~ with open(file_, 'rb') as f:
+                    #~ f.seek(0, os.SEEK_END)
+                    #~ size = f.tell()
+                    #~ nt = (size-3600.0)/(240.0+ns*4.0)
+                    #~ assert nt % 1 == 0
+                #~ return nt
+
+        #~ def ibm2ieee(ibm):
+            #~ s = ibm >> 31 & 0x01 
+            #~ exp = ibm >> 24 & 0x7f 
+            #~ fraction = (ibm & 0x00ffffff).astype(np.float32) / 16777216.0
+            #~ ieee = (1.0 - 2.0 * s) * fraction * np.power(np.float32(16.0), exp - 64.0) 
+            #~ return ieee
         
-        ibmtype = np.dtype(segy_trace_header_dtype.descr + [('data', ('>i4',ns))])
-        ieeetype = np.dtype(segy_trace_header_dtype.descr + [('data', ('f4',ns))])
-        for index, file_ in enumerate(filelist):
-            th = self.db.createTable(node, "gather"+str(index), ieeetype, "Gather")
+        
+        #~ node = self.db.createGroup("/", 'jobname', 'PySeis Node')
+        #~ fh = self.db.createTable(node, 'FH', segy_textual_header_dtype, "EBCDIC File Header")
+        #~ bh = self.db.createTable(node, 'BH', segy_binary_header_dtype, "Binary File Header")
+        #~ th = self.db.createTable(node, "TH", segy_trace_header_dtype, "Headers")
+        #~ td = self.db.createTable(node, "TD", np.dtype([('data', ('f4',ns))]), "Headers")
+
+        #~ for file_ in filelist:
+            #~ EBCDIC = read_EBCDIC(file_)
+            #~ if EBCDIC: fh.append(EBCDIC)
+            #~ bheader = read_bheader(file_)
+            #~ bh.append(bheader)
+            #~ self.db.flush()
+
+            #~ assert np.any(np.diff(bh.cols.hns[:])) == False #check ns is constant
+            #~ assert np.any(np.diff(bh.cols.hdt[:])) == False #check ns is constant
+            #~ ns = bh.cols.hns[0]
+            #~ dt = bh.cols.hdt[0]
+
+        #~ for index, file_ in enumerate(filelist):
+
             #will need to add chunking at some point.
-            counts = 0
-            with open(file_, 'rb') as f:
-                f.seek(3600+240)
-                try:
-                    while True:
-                        data = np.fromfile(f, dtype=ibmtype, count=int(1e5))
-                        data['data'] = ibm2ieee(data['data'])
-                        th.append(data.astype(ieeetype))
-                        th.flush()
-                except:
-                    data = np.fromfile(f, dtype=ibmtype)
-                    data['data'] = ibm2ieee(data['data'])
-                    th.append(data.astype(ieeetype))
-                    th.flush()
-                    
+            #~ counts = 0
+            #~ with open(file_, 'rb') as f:
+                #~ f.seek(3600+240)
+                #~ try:
+                    #~ while True:
+                        #~ data = np.fromfile(f, dtype=ibmtype, count=int(1))
+                        #~ data['data'] = ibm2ieee(data['data'])
+                        #~ th.append(data.astype(ieeetype))
+                        #~ th.flush()
+                #~ except:
+                    #~ data = np.fromfile(f, dtype=ibmtype)
+                    #~ data['data'] = ibm2ieee(data['data'])
+                    #~ th.append(data.astype(ieeetype))
+                    #~ th.flush()
+                        
 
             
 
 
 if __name__ == '__main__':
     os.chdir('../../data/')
-    path = '/misc/FieldData/Job_A023_Origin_Hi_res_trial_2013/PHASE1/VIBE_SEGY/'
+    path = '/coalStor/University_QLD/crystal_mountain/2014/data/segy/'
     filelist = [path+a for a in os.listdir(path) if '.sgy' in a]
-    a = segy('segy_test.h5')
+    a = database('segy_test.h5')
     a.read(filelist)
 
 
