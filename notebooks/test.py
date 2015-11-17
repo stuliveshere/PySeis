f96edcf2d3453a38d95950789a8697c3adcde903
Stu Fletcher <mail@stuliveshere.com>
2015-11-17 19:52:06 +1000

    testing plotting wiggles

--- notebooks/test.py
+++ notebooks/test.py
@@ -1,34 +1,45 @@
 
 
 
-from toolbox import typeSU
+import toolbox
 import numpy as np
 from matplotlib import collections
 from timeit import Timer
+import pylab
 
 def func(panel): 
     
     panel['trace'][:,-1] = np.nan
     trace_centers = np.linspace(1,284, panel.size).reshape(-1,1)
-    scalar = 284/(panel.size*0.5)
+    scalar = 284/(panel.size*0.1)
     y = np.meshgrid(np.arange(1501), np.arange(284))[0].ravel() 
     offsets = (np.meshgrid(np.arange(1501), np.arange(284))[1]+1).ravel()
     x = ((panel['trace']*scalar)+trace_centers).ravel()
+    xlines = np.split(x, 284)
+    ylines = np.split(y, 284)
+    lines = [zip(xlines[a],ylines[a]) for a in range(len(xlines))]  
     fig,ax = pylab.subplots()
     ax.fill_betweenx(y,offsets,x,where=(x>offsets),color='k')
+    col1 = collections.LineCollection(lines)
+    col1.set_color('k')
+    ax.add_collection(col1, autolim=True)
     pylab.xlim([0,284])
     pylab.ylim([0,1500])
     ax.set_ylim(ax.get_ylim()[::-1])
     pylab.tight_layout()
-    pylab.show(block=False)
+    pylab.show()
 
+file = "/home/sfletcher/Downloads/2d_land_data/2D_Land_data_2ms/Line_001.su"
+data, params = toolbox.initialise(file)
 dmap = np.memmap(file, dtype=toolbox.typeSU(1501), mode='r')
 eps = np.unique(dmap['ep'])
 for ep in eps[:1]:
+    params['window'] = 500
     panel = dmap[dmap['ep'] == ep].copy()
     panel = toolbox.agc(panel, None, **params)
-    t = Timer("""func(panel)""", setup="from __main__ import func; from __main__ import panel")
-    print t.timeit(100)
+    func(panel)
+    #t = Timer("""func(panel)""", setup="from __main__ import func; from __main__ import panel")
+    #print t.timeit(100)
 
 
 #777.01802206
--- notebooks/test1.py
+++ notebooks/test1.py
@@ -1,47 +1,74 @@
 import numpy as np
 from matplotlib import collections
 import toolbox
-dmap = np.memmap(file, dtype=toolbox.typeSU(1501), mode='r')
+import pylab
+
+file = "/home/sfletcher/Downloads/2d_land_data/2D_Land_data_2ms/Line_001.su"
+data, params = toolbox.initialise(file)
+dmap = data #np.memmap(file, dtype=toolbox.typeSU(1501), mode='r')
 eps = np.unique(dmap['ep'])
 for ep in eps[:1]:
+    params['window'] = 500
     panel = dmap[dmap['ep'] == ep].copy()
+    print 'doing agc'
     panel = toolbox.agc(panel, None, **params)
-
+    print 'starting plot'
     trace_centers = np.linspace(1,284, panel.size).reshape(-1,1)
-    scalar = 284/(panel.size*0.5)
+    scalar = 284/(panel.size*0.1)
     panel['trace'][:,-1] = np.nan
-    x = panel['trace'].ravel()
-    x[x < 0] = 0
-    y = np.meshgrid(np.arange(1501), np.arange(284))[0].ravel() 
+    x = panel['trace'].copy()
+    x[x< 0] = np.nan
     
-    zero_crossings = np.where(x == 0)[0]+1
-    zero_crossings = zero_crossings[np.diff(zero_crossings) == 1]
+    xx = ((x*scalar)+trace_centers).flatten().tolist()
+    y = np.meshgrid(np.arange(1501), np.arange(284))[0].ravel().tolist(
+    
+    for i in range(1, len(xx), 1501):
+        xx[i] = None
+        y[i] = None
+    
+    #~ pylab.plot(x)
+    #~ pylab.plot(np.zeros_like(x))
+    #~ pylab.show()
+    
+    #~ inds = np.diff([x > 0])[0]
+    
+    #x[x < 0.0].fill(0.0)
+    #x = [x > 0]
+    #zero_crossings = np.where(x == 0)[0]+1
+    #zero_crossings = zero_crossings[np.diff(zero_crossings) !=0 ]# 1]
     #zero_crossings = np.where(np.diff(np.signbit(x)))[0]+1
+    #~ x = ((panel['trace']*scalar)+trace_centers).ravel()
+    #~ xverts = np.split(x, inds)[::2]
+    #~ yverts = np.split(y, inds)[::2]
+    #~ polygons = [zip(xverts[i], yverts[i]) for i in range(0, len(xverts)) if len(xverts[i]) > 2]
     
-    x = ((panel['trace']*scalar)+trace_centers).ravel()
 
-    xverts = np.split(x, zero_crossings)
-    yverts = np.split(y, zero_crossings)
     
-    
-    polygons = [zip(xverts[i], yverts[i]) for i in range(0, len(xverts)) if len(xverts[i]) > 2]
-    
-    xlines = np.split(x, 284)
-    ylines = np.split(y, 284)
+    #print polygons.shape
+    #print polygons
+    x1 = ((panel['trace']*scalar)+trace_centers).ravel()
+    y1 = np.meshgrid(np.arange(1501), np.arange(284))[0].ravel()
+    xlines = np.split(x1, 284)
+    ylines = np.split(y1, 284)
     lines = [zip(xlines[a],ylines[a]) for a in range(len(xlines))]  
 
-
+    #~ lines = np.column_stack([x,y])
+    #~ print lines
+    #~ print lines.shape
+    #~ print lines.ndim
+    #~ print 'plotting'
     fig = pylab.figure()
     ax = fig.add_subplot(111)
-    col = collections.PolyCollection(polygons)
-    col.set_color('k')
-    ax.add_collection(col, autolim=True)
+    #~ col = collections.PolyCollection(polygons)
+    #~ col.set_color('k')
+    #~ ax.add_collection(col, autolim=True)
     col1 = collections.LineCollection(lines)
     col1.set_color('k')
     ax.add_collection(col1, autolim=True)
     ax.autoscale_view()
+    pylab.fill(xx,y, 'k')
     pylab.xlim([0,284])
     pylab.ylim([0,1500])
     ax.set_ylim(ax.get_ylim()[::-1])
     pylab.tight_layout()
-    pylab.show(block=false)
+    pylab.show()
