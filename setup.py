73a1bf8e60452816cb5c3fdb9045dd2f7d506512
Stu Fletcher <mail@stuliveshere.com>
2016-11-07 22:20:51 +1000

    tipped the bucket into the package. cant walk on the floor now...

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
-from setuptools import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+    
+config = {
+  'name':'PySeis',
+  'version':'0.1',
+  'description':'Seismic Processing Package',
+  'url':'http://github.com/stuliveshere/PySeis',
+  'author':'Stewart Fletcher',
+  'author_email':'mail@stuliveshere.com',
+  'license':'MIT',
+  'packages':['PySeis'],
+  'zip_safe':False
+  }
 
-setup(name='PySeis',
-      version='0.1',
-      description='Seismic Processing Package',
-      url='http://github.com/stuliveshere/PySeis',
-      author='Stewart Fletcher',
-      author_email='mail@stuliveshere.com',
-      license='MIT',
-      packages=['PySeis'],
-      zip_safe=False)
+setup(**config)
---
+++ tests/PySeis_tests.py
@@ -0,0 +1,10 @@
+import PySeis
+
+def setup():
+    print "SETUP!"
+
+def teardown():
+    print "TEAR DOWN!"
+
+def test_basic():
+    print "I RAN!"
