9b0e410a18e136f58f9f859bed12462ac9c7e48a
Stu Fletcher <mail@stuliveshere.com>
2016-11-08 21:40:27 +1000

    added .npy support to container

--- PySeis/core/container.py
+++ PySeis/core/container.py
@@ -1,25 +1,48 @@
-import format
+import format as su
 import numpy as np
 
 class workspace(object):
     '''
-    holding class for launching su file
+    holds the data object and associated methods
     '''
     def __init__(self):
-        self.ns = None
-        self.dtype = None
         self.data = None
         
-    def initialise(self, filename, fileType="su"):
-        if fileType == "su":
-            self.ns = self.getNs(filename)
-            self.dtype = np.dtype(format.traceHeaderDtype.descr + [('data', ('<f4',self.ns))])
-            self.data =  np.memmap(filename, dtype=self.dtype, mode='r')
+    def load(self, file, format="npy"):
+        '''
+        initialises a file
+        i.e. memmaps the SU file to a numpy array.
+        '''
+        if format == "npy":
+            self.data = np.load(file, mmap_mode='r')
+        if format == "su":
+            _ns = self.getNs(file)
+            _type =self. build_dtype(_ns)
+            self.data =  np.memmap(file, dtype=_type, mode='r')
         
-    def getNs(self, filename):
-        return np.fromfile(filename, dtype=format.traceHeaderDtype, count=1)['ns']
+    def getNs(self, file):
+        '''
+        reaches into the SU file and reads the number of samples
+        fom the first trace header.
+        '''
+        return np.fromfile(file, dtype=su.traceHeaderDtype, count=1)['ns']
+    
+    def build_dtype(self, _ns):
+        '''
+        builds a numpy dtype as defined
+        in format. 
+        '''
+        return np.dtype(su.traceHeaderDtype.descr + [('data', ('<f4',_ns))])
         
-    def new(self, ns):
+    def new(self, _ns):
         pass
+        
+    def save(self, file, format="npy"):
+        if format == "npy":
+            np.save(file, self.data)
+        if format == "su":
+            self.data.tofile(file)
+        
     
-    
+if __name__ == "__main__":
+    pass
--- tests/PySeis_tests.py
+++ tests/PySeis_tests.py
@@ -1,17 +1,28 @@
 from nose import with_setup 
-import numpy
+import numpy as np
 import unittest
 
-import PySeis
 
+import PySeis
 class ContainerTests(unittest.TestCase):
+    
     def setUp(self):
-        self.suFile = './data/sample.su'
+        self.file = './data/sample.su'
  
     def test_initialise_SU_file(self):
         data = PySeis.workspace()
-        data.initialise(self.suFile)
-        self.assertIsInstance(data.data,numpy.ndarray)
+        data.load(self.file, format="su")
+        self.assertIsInstance(data.data,np.ndarray)
+        data.save('./data/sample.npy')
+        
+        data1 = PySeis.workspace()
+        data1.load('./data/sample.npy')
+        #for name in data1.data.dtype.names:
+            #if name != "data":
+                #self.assertTrue((data.data[name] == data1.data[name]).all())
+        #self.assertTrue((data.data.tolist() == data1.data.tolist()).all())
+        
+        
         
         
         
