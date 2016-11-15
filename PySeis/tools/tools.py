1eb019b8a746514cbc00a1ad69fb9cc5409bc908
Stu Fletcher <mail@stuliveshere.com>
2016-11-15 17:03:01 +1000

    added tool subpackage

---
+++ PySeis/tools/tools.py
@@ -0,0 +1,18 @@
+import time        
+
+
+#==================================================
+#                                 timing decorator
+#==================================================
+def timeit(method):
+
+    def timed(*args, **kw):
+        ts = time.time()
+        result = method(*args, **kw)
+        te = time.time()
+
+        print '%r (%r, %r) %2.2f sec' % \
+              (method.__name__, args, kw, te-ts)
+        return result
+
+    return timed
