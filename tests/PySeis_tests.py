451e8d623b265a5ef4ef1ce5462b1f5b463de066
Stu Fletcher <mail@stuliveshere.com>
2016-11-15 11:06:17 +1000

    updated chunker test to apply agc

--- tests/PySeis_tests.py
+++ tests/PySeis_tests.py
@@ -8,17 +8,17 @@ from PySeis import Stream
 class ContainerTests(unittest.TestCase):
     
     def setUp(self):
-        self.infile = './data/sample.su'
-        self.outfile = './data/sample.npy'
+        self.path = "/home/sfletcher/Alaska/"
+
  
     def test_initialise_SU_file(self):
-        PySeis.loadSU(self.infile, self.outfile)
-        data = Stream(self.outfile, "./data/test.npy")
+        PySeis.loadSU(self.path+"raw.su", self.path+"raw.npy")
+        data = Stream(self.path+"raw.npy", self.path+"agc.npy")
         params = {}
         for gather in data:
             PySeis.processing.toolbox.agc(gather, **params)
             gather.save()
-        PySeis.saveSU("./data/test.npy", "./data/test.su")
+        PySeis.saveSU(self.path+"agc.npy", self.path+"agc.su")
 
         
         
