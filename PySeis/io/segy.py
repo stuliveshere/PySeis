8d69a66cf58d074bec03f14deb4cfac5be25c969
Stu Fletcher <mail@stuliveshere.com>
2017-07-17 16:10:36 +1000

    modifying segy read to handle chunks of fixed memory size

--- PySeis/io/segy.py
+++ PySeis/io/segy.py
@@ -163,19 +163,28 @@ def num_traces(_file, ns):
 		assert nt % 1 == 0
 	return nt
 	
-def read_traces(_file, ns, chunksize=1000):
+def read_traces(_file, ns, chunksize=0.01): 
 	#assuming ibm floats for now
+	#chunksize in Gb
+	ibmtype = np.dtype(segy_trace_header_dtype.descr + [('data', ('<i4',ns))])
+	ftype = np.dtype(segy_trace_header_dtype.descr + [('data', ('<f4',ns))])
 	with open(_file, 'rb') as f:
 		#some trickery to calculate filesize
 		f.seek(0, os.SEEK_END)
-		filesize = f.tell()
+		filesize = f.tell() #filesize in bytes
 		#caculate size of traces
 		tracesize = 240+ns*4.0
 		ntraces = (filesize-3600)/tracesize
+		
+		chunks = min(filesize, np.floor(filesize/(chunksize*1.0e9)))
+		tracesPerChunk = min(ntraces, np.floor(chunksize*1.0e9/tracesize))
+		chunksize = tracesPerChunk*tracesize
+		remainder = divmod(filesize-3600, chunksize)
+		
+			
+		
+		
 		f.seek(3600)    #start at end of binary header block
-		chunks, remainder = divmod(ntraces, chunksize)
-		ibmtype = np.dtype(segy_trace_header_dtype.descr + [('data', ('<i4',ns))])
-		ftype = np.dtype(segy_trace_header_dtype.descr + [('data', ('<f4',ns))])
 		for n in range(int(chunks+1)):
 			if n ==int(chunks+1): chunksize = remainder #last chunk
 			data = np.fromfile(f, dtype=ftype, count=int(chunksize))
