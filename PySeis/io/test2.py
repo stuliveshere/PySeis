98f73428308a67deb92f551f6e801ab543796c82
stu fletcher <mail@stuliveshere.com>
2018-11-20 13:06:02 +1000

    moved test to test2

---
+++ PySeis/io/test2.py
@@ -0,0 +1,32 @@
+'''
+Created on 20 Nov. 2018
+
+@author: sfletcher
+'''
+
+class Chunker(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
+    def blockHeaders(self):
+        pass
+        
+            
+    def chunker(self):
+        pass
+
+        
+    def read(self):
+        pass
+    
+class Segy(Chunker):
+    def blockHeaders(self):
+        pass
+    
