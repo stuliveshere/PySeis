5dac678d4f2d0b9b85adccd38008d974c37930d5
stu fletcher <mail@stuliveshere.com>
2023-05-26 17:03:56 +1000

    added order flag to allow manual endian control.

--- PySeis/io/tools.py
+++ PySeis/io/tools.py
@@ -1,24 +1,50 @@
 import numpy as np
 
-def pack_dtype(_dtype=None, values=[]): #name, format, offset
-	names = []
-	formats = []
-	offsets = []
-	if _dtype:
-		names = list(_dtype.fields.keys())
-		for key in names:
-			formats.append(_dtype.fields[key][0])
-			offsets.append((_dtype.fields[key][1]))
-	if values:
-		for entry in values:
-			names.append(entry[0])
-			formats.append(entry[1])
-			offsets.append(entry[2])
-	return np.dtype({
-	'names': names,
-	'formats': formats,
-	'offsets': offsets,
-	})
+def pack_dtype(_dtype=None, values=[], order=">"): 
+    """
+    This function constructs a new numpy dtype object based on an existing dtype 
+    and a list of additional field specifications. The dtype can also be set 
+    to use a specific byte order.
+
+    Parameters:
+    _dtype (np.dtype, optional): Existing numpy dtype object.
+    values (list of tuple, optional): List of field specifications.
+    order (str, optional): Byte order for the data.
+
+    Returns:
+    np.dtype: New numpy dtype object.
+    """
+    
+    # Initialize lists to store names, formats and offsets
+    names = []
+    formats = []
+    offsets = []
+    
+    # If an existing dtype object is provided, extract its field specifications
+    if _dtype:
+        names = list(_dtype.fields.keys())
+        for key in names:
+            formats.append(_dtype.fields[key][0])
+            offsets.append((_dtype.fields[key][1]))
+    
+    # If additional field specifications are provided, append them to the lists
+    if values:
+        for entry in values:
+            names.append(entry[0])
+            formats.append(entry[1])
+            offsets.append(entry[2])
+    
+    # Construct a new dtype object using the specifications
+    new_dtype = np.dtype({
+        'names': names,
+        'formats': formats,
+        'offsets': offsets,
+    })
+    
+    # Return the new dtype object with the specified byte order
+    return new_dtype.newbyteorder(order)
+
+
 
 def memory():
 	"""
